bdb2020a710c83a03a9acee0204d7e771fa52fc5
Mark <mark@iMac2.local>
2018-09-17 15:08:15 -0400

    created the chatbot and made it respond to user input.

---
+++ assignment_two.py
@@ -0,0 +1,13 @@
+#mark hooks unit two assignment
+#this program is to create a chatbot
+
+chatbot_name = "Not A.I Bot"
+print("Hello! I am Not A.I Bot!")
+users_name = input("What is your name")
+print("What a lovely name!")
+users_place_of_origin = input("Where are you from?")
+print("What a lovely place to live!")
+users_favorite_number = input("What is your favorite number?")
+print("What a great number")
+users_dream_car = input("What is your dream car?")
+print("What a great car! I wish i had a car.")
